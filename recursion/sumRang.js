29b972b0b77ff5ce0a05b4b91a9930a23d4abad4
Awais Ahmad <awais6412l@gmail.com>
2023-01-25 09:47:47 +0500

    add sumRang recursion

--- recursion/sumRang.js
+++ recursion/sumRang.js
@@ -1,19 +1,19 @@
 // sumrang without recursion
 console.log('sumrang without recursion');
-      function SumRang(number) {
-        let result = 0;
-        for (let index = number; index > 0; index--) {
-          result = result + index;
-        }
-        return result;
-      }
-      console.log(SumRang(10));
-// sumrang with recursion
-// console.log('sumrang with recursion');
-// function sumRang(num, total = 0) {
-//   if (num <= 0) {
-//     return total;
-//   }
-//   return num * sumRang(num - 1, total + num);
-// }
-// console.log(sumRang(10));
+function SumRang(number) {
+  let result = 0;
+  for (let index = number; index > 0; index--) {
+    result = result + index;
+  }
+  return result;
+}
+console.log(SumRang(10));
+//sumrang with recursion
+console.log('sumrang with recursion');
+function sumRang(num, total = 0) {
+  if (num <= 0) {
+    return total;
+  }
+  return  sumRang(num - 1, total + num);
+}
+console.log(sumRang(10));
