fb1b0a97c8cf472cbfdee520835cd16d09cea015
Awais Ahmad <awais6412l@gmail.com>
2024-01-25 20:39:49 +0500

    updated code comments

--- basic-js/es6-destructing/fizzbuzz.js
+++ basic-js/es6-destructing/fizzbuzz.js
@@ -1,9 +1,9 @@
 /*
 FizzBuzz is probably the most common interview question for entry level developers. At least, it used to be. It may not be used as much because of how common it was. It's a simple problem that tests your ability to think logically and write clean code.
 
-Traditionally, you loop from 1 to 100 and print out each number. However, if the number is divisible by 3, you print out "Fizz" instead. If the number is divisible by 5, you print out "Buzz" instead. If the number is divisible by both 3 and 5, you print out "FizzBuzz" instead.
+Traditionally, you loop from 1 to 100 and print out each number. However, if the number is divisible by 3, you print out "Fizz" instead. If the number is divisible by 5, you replace out "Buzz" instead. If the number is divisible by both 3 and 5, you replace out "FizzBuzz" instead.
 
-In this challenge, you will write a function called fizzBuzz that takes in a number.  However, if the number is divisible by 3, you should log "Fizz". If the number is divisible by 5, you should log "Buzz". If the number is divisible by both 3 and 5, you should log "FizzBuzz".
+In this challenge, you will write a function called fizzBuzz that takes in a number and return an array However, if the number is divisible by 3, you should add "Fizz" in an array. If the number is divisible by 5, you should add "Buzz" in an array. If the number is divisible by both 3 and 5, you should add "FizzBuzz".
 */
 
 const fizzBuzz = (n) => {
@@ -11,19 +11,19 @@ const fizzBuzz = (n) => {
   // Loop through the numbers from 1 to the number passed in
   for (let i = 1; i <= n; i++) {
     // If the number is divisible by 3
-    //p;o;[-and 5, print 'FizzBuzz'
+    // and 5, add 'FizzBuzz'
     if (i % 5 === 0 && i % 3 === 0) {
       result.push('FizzBuzz');
     }
-    // If the number is divisible by 3, print 'Fizz'
+    // If the number is divisible by 3, add 'Fizz'
     else if (i % 3 === 0) {
       result.push('Fizz');
     }
-    // If the number is divisible by 5, print 'Buzz'
+    // If the number is divisible by 5, add 'Buzz'
     else if (i % 5 === 0) {
       result.push('Buzz');
     }
-    // otherwise, print i
+    // otherwise, add i
     else {
       result.push(i);
     }
